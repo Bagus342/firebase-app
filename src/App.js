eccf0c510bc0faeed3cb6dd05553b92637a995d5
Bagus342 <bagusyes005@gmail.com>
2024-06-18 15:55:03 +0700

    create repo, create feature login & regist, and update profile user

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import ResponsiveAppBar from './components/Navbar';
+import Home from './containers/Home';
+import { Outlet } from 'react-router-dom';
+import { ThemeProvider } from '@emotion/react';
+import { createTheme } from '@mui/material';
+
+const defaultTheme = createTheme()
 
 function App() {
   return (
+    <ThemeProvider theme={defaultTheme}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ResponsiveAppBar />
+      <Outlet />
     </div>
+    </ThemeProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import SignIn from './containers/SignIn';
+import SignUp from './containers/SignUp';
 import reportWebVitals from './reportWebVitals';
+
+import { BrowserRouter, Routes, Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import PrivateComponent from './components/ProtectedRoute';
+import Profile from './containers/Profile';
+import Home from './containers/Home';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/" element={
+            <PrivateComponent>
+              <Home />
+            </PrivateComponent>
+          } />
+          <Route path='profile' element={
+            <PrivateComponent>
+              <Profile />
+            </PrivateComponent>
+          } />
+        </Route>
+        <Route path="signin" element={
+          <PrivateComponent loginOnly={false}>
+            <SignIn />
+          </PrivateComponent>
+        } />
+        <Route path="signup" element={
+          <PrivateComponent loginOnly={false}>
+            <SignUp />
+          </PrivateComponent>
+        } />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
